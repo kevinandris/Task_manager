4d539478c383d1aeaf316a139c7ff3725865a8fc
keipy27 <kevinandris27@gmail.com>
2023-08-08 11:34:54 +1200

    adding initial state object and useReducer to TaskManagerReducer.js

--- src/components/taskManager/TaskManagerReducer.js
+++ src/components/taskManager/TaskManagerReducer.js
@@ -1,19 +1,30 @@
 // ! Parent class
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect, useReducer } from 'react'
 import useLocalStorage from 'use-local-storage'
 import './TaskManager.css'
 import Task from './Task'
 import Alert from '../alert/Alert'
 import Confirm from '../confirm/Confirm'
 
-const TaskManagerReducer = () => {
+const taskReducer = (state, action) => {};
 
+
+const TaskManagerReducer = () => {
+  
   const [name, setName] = useState("")
   const [date, setDate] = useState("")
   const [tasks, setTasks] = useLocalStorage("tasks", [])
   
-  const [taskID, setTaskID] = useState(null)
-  const [isEditing, setIsEditing] = useState(false);
+  const initialState = {
+    tasks,
+    taskID: null,
+    isEditing: false,
+    isAlertOpen: false,
+    alertContent: "This is an alert",
+    alertClass: "success"
+  }
+
+  const [state, dispatch] = useReducer(taskReducer, initialState)
 
   const nameInputRef = useRef(null)
 
@@ -25,79 +36,28 @@ const TaskManagerReducer = () => {
     // preventing the page from refreshing
     e.preventDefault();
 
-    // To check if values are stored
-    console.log(name)
-    console.log(date)
-
-    if (!name && !date || !name || !date) {
-      alert("Please enter task name and date");
-    } else if (name && date && isEditing) {
-      setTasks(
-        tasks.map((task) => {
-          if (task.id === taskID) {
-            return {...task, name, date, complete: false}
-          }
-
-          return task;
-        })
-      )
-      setName("")
-      setDate("")
-      setIsEditing(false)
-      setTaskID(null)
-    } else {
-      const newTask = {
-        id: Date.now(),
-        name,
-        date,
-        complete: false,
-      }
-
-      console.log(newTask);
-
-      setTasks([...tasks,newTask])
-      setName("")
-      setDate("")
-    }
   };
 
   //* Push (edit) the task to the task and date input when edit icon is clicked
   const editTask = (id) => {
-    const thisTask = tasks.find((task) => task.id === id)
-    setIsEditing(true)
-    setTaskID(id)
-    setName(thisTask.name)
-    setDate(thisTask.date)
+    
   }
 
   // delete function
   const deleteTask = (id) => {
-    // to check the id for every data
-    console.log(id);
-
-    if (window.confirm("Delete this task") === true) {
-      const newTask = tasks.filter((task) => task.id !== id)
-      setTasks(newTask);
-    }
+    
   }
 
   // complete function
   const completeTask = (id) => {
-    setTasks(
-      tasks.map((task) => {
-        if (task.id === id) {
-          return {...task, complete: true}
-        }
-
-        return task;
-      })
-    )
+    
   }
   
   return (
     <div className='--bg-primary'>
-      {/* <Alert /> */}
-      <Confirm />
+      {state.isAlertOpen && <Alert /> }
+      
+      {/* <Confirm /> */}
       <h2 className='--text-center --text-light'>Task Manager Reducer</h2>
 
       <div className='--flex-center --p'>
@@ -115,7 +75,7 @@ const TaskManagerReducer = () => {
             <input type="date" placeholder='Task name' name='date' value={date} onChange={(e) => setDate(e.target.value)}/>
           </div>
 
-          <button className="--btn --btn-success --btn-block">{isEditing ? "Edit Task" : "Save Task"}</button>
+          {/* <button className="--btn --btn-success --btn-block">{isEditing ? "Edit Task" : "Save Task"}</button> */}
         </form>
 
        </div>
