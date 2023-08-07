866bf9b88bf02e540b8dd5f06209697db37a2e3a
keipy27 <kevinandris27@gmail.com>
2023-08-07 12:11:09 +1200

    Adding Task.js code that lists all the tasks and equipped with icons

--- src/components/taskManager/Task.js
+++ src/components/taskManager/Task.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { FaCheckDouble, FaEdit, FaTrashAlt} from "react-icons/fa"
 
 const Task = () => {
   return (
-    <div>Task</div>
+    <div className='task'>
+      <span>
+        <p><b>Task</b> Task one</p>
+        <p><b>Date</b> 2022-02-22</p>
+      </span>
+
+      <span>
+        <button>
+          <FaEdit color='green'/>
+        </button>
+
+        <button>
+          <FaTrashAlt color='red'/>
+        </button>
+
+        <button>
+          <FaCheckDouble color='purple'/>
+        </button>
+      </span>
+    </div>
   )
 }
 
--- src/components/taskManager/TaskManager.js
+++ src/components/taskManager/TaskManager.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './TaskManager.css'
+import Task from './Task'
 
 const TaskManager = () => {
   return (
     <div className='--bg-primary'>
       <h1 className='--text-center --text-light'>Tasks Manager</h1>
+
       <div className='--flex-center --p'>
        <div className="--card --bg-light --width-500px --p --flex-center">
 
@@ -15,7 +17,7 @@ const TaskManager = () => {
           </div>
           
           <div>
-            <label htmlFor="name">Task:</label>
+            <label htmlFor="name">Date:</label>
             <input type="date" placeholder='Task name' name='date'/>
           </div>
 
@@ -24,6 +26,17 @@ const TaskManager = () => {
 
        </div>
       </div>
+
+      {/* DISPLAY TASK */}
+      <article className='--flex-center --my2'>
+        <div className="--width-500px --p">
+          <h2 className='--text-light'>Task List</h2>
+          <hr style={{ background: "#fff" }}/>
+
+          <Task />
+        </div>
+      </article>
+
     </div>
   )
 }
